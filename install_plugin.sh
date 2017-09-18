a23aefd0f063d8b3fe27740303da18bb8654acbb
ezioruan <qiaoqinqie2@gmail.com>
2017-09-18 09:58:39 +0800

    fix

--- install_plugin.sh
+++ install_plugin.sh
@@ -2,7 +2,7 @@ pip install --upgrade autopep8
 go get -u -v github.com/nsf/gocode 
 go get -u -v code.google.com/p/rog-go/exp/cmd/godef 
 go get -u -v github.com/bradfitz/goimports 
-sudo npm install -g jsfmt eslint babel-eslint eslint-plugin-react esformatter
+sudo npm install -g eslint babel-eslint eslint-plugin-react esformatter eslint-plugin-standard eslint-plugin-promise
 cp ./.esformatter ~/
 cp ./.eslintrc.js ~/
 
