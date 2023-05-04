cffa96de7285bc5c6f19b071a5e24026000eae4b
ezioruan <ezioruan@gmail.com>
2023-05-04 16:06:29 +0800

    update

--- install_plugin.sh
+++ install_plugin.sh
@@ -3,4 +3,6 @@ sudo npm install -g eslint babel-eslint eslint-plugin-react esformatter eslint-p
 cp ./.esformatter ~/
 cp ./.eslintrc.js ~/
 curl -sSL https://install.python-poetry.org | python3 -
+git clone https://github.com/github/copilot.vim.git \
+  ~/.vim/pack/github/start/copilot.vim
 
