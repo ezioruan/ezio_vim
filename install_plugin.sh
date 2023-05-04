1bf51f1d5f4f1a8e5638780d899fb2e5cd2018ae
ezioruan <ezioruan@gmail.com>
2023-05-04 16:02:32 +0800

    update

--- install_plugin.sh
+++ install_plugin.sh
@@ -2,5 +2,5 @@ pip install --upgrade autopep8
 sudo npm install -g eslint babel-eslint eslint-plugin-react esformatter eslint-plugin-standard eslint-plugin-promise
 cp ./.esformatter ~/
 cp ./.eslintrc.js ~/
-
+curl -sSL https://install.python-poetry.org | python3 -
 
